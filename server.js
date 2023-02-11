e4129c857e26554adc3779a00906b71d72ca446f
BuildTools <unconfigured@null.spigotmc.org>
2023-02-11 12:57:52 -0700

    Added sequelize stuff to the package.json, server.js, and .env

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+const { Sequelize } = require('sequelize')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4002;
@@ -7,6 +8,22 @@ const port = process.env.PORT || 4002;
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'my_password'
+  })
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
 
 // ROOT
 app.get('/backend', (req, res) => {
