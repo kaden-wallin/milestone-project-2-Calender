ca4f93db9e585d3f83727dea73eadfaed3387893
Gia Madonna <giamadonna1@gmail.com>
2023-02-13 17:33:37 -0800

    Got db right. Need to figure out a bug for the friends table

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = new Sequelize({
     storage: process.env.PG_URI,
     dialect: 'postgres',
     username: 'postgres',
-    password: 'my_password'
+    password: process.env.PG_PASSWORD
   })
 
 try {
