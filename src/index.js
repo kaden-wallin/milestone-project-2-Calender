a823db50b0c6480497a823d7819fda71e61b9392
Gia Madonna <giamadonna1@gmail.com>
2023-02-14 14:04:42 -0800

    Added the SupaBase connection without the anon key

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createClient } from '@supabase/supabase-js';
 import './index.css';
 import App from './App';
 
@@ -18,3 +19,13 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+
+
+const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
+
+async function getCountries() {
+  const countries = await supabase.from('countries').select()
+  console.log(countries)
+}
+
+getCountries()
