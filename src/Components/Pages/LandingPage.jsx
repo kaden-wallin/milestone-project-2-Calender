90fa8dcc228b68b6fbaf6e0ca6654f17ff9e4aff
Gia Madonna <giamadonna1@gmail.com>
2023-02-17 15:06:28 -0800

    Not functional. Don't merge

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -1,13 +1,53 @@
 import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
+import { useUser , useSupabaseClient } from "@supabase/auth-helpers-react"
+
+
+
+
 
 function LandingPage() {
-  return (
+  const [ email, setEmail ] = useState("");
+  const user = useUser();
+  const supabase = useSupabaseClient();
+  console.log(email)
+
+  async function sendLoginLink() {
+    const { data, error } = await supabase.auth.signInWithOtp({
+      email: email
+    })
+
+    if(error) {
+      alert("Error. Please retry Email Address!")
+      console.log(error)
+    } else {
+      alert("Please check your email ")
+    }
+    }
+  
+  
+  return ( 
     <div className=' text-center' >
+      { user === null ?
+      <>
+      <h1> Welcome to The Calndaer </h1>
+      <form>
+        <label htmlFor='Email' type= 'email' onChange={(e) => setEmail(e.target.value)}> Enter your email here </label>
+        <input id='user_email' type='text'></input>
+        <Button onClick={() => sendLoginLink()}> Get Your Login Link! </Button>
+      </form>
+      </>
+      :
+      <>
+      <h1> Welcome back! </h1>
         <Link to = '/choose' >
             <button varientcolor = 'blue' className = '-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 t  text-green-500' type = 'button'>Start</button>
         </Link>
+      </>
+      }
     </div>
   )
-}
+    }
 
 export default LandingPage
