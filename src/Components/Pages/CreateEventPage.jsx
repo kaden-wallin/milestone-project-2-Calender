b54eea4247ece3303f0e923c1461eea3d3893027
BuildTools <unconfigured@null.spigotmc.org>
2023-02-21 20:38:15 -0700

    Idk why the models and controllers switched back but they are better now

--- src/Components/Pages/CreateEventPage.jsx
+++ src/Components/Pages/CreateEventPage.jsx
@@ -1,39 +1,8 @@
 import GoBackBtn from '../GoBackBtn'
 
 function CreateEventPage() {
-  function handleSubmit(event) {
-    event.preventDefault()
-
-    // Extract the form data
-    const formData = {
-      title: document.getElementById('event-title').value,
-      date: document.getElementById('choose-date').value,
-      location: document.getElementById('location').value,
-    }
-
-    // Make the API call
-    fetch('/api/events', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    })
-      .then(response => {
-        if (response.ok) {
-          alert('Event added successfully!')
-          // TODO: Reset the form
-        } else {
-          alert('Failed to add event')
-        }
-      })
-      .catch(error => {
-        alert('An error occurred while adding the event')
-        console.error(error)
-      })
-  }
-
   return (
+
     <div className=' text-center  m-auto w-auto items-center  justify-between font-bold '>
       <GoBackBtn />
       <form onSubmit={handleSubmit} className= 'items-center justify-center rounded-md text-center t bg-gray-50 text-2xl'>
@@ -45,7 +14,6 @@ function CreateEventPage() {
         <input id="location" type="text" />
         <input type="submit" value="Add Event" />
       </form>
-
     </div>
   )
 }
--- src/migrations/20230211212414-create-report.js
+++ src/migrations/20230211212414-create-report.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('reported_users', {
-        report_user_id: {
+        reported_user_ID: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
