d4f6bed36677167d05bfc08e574efecf9994036e
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-02-22 15:59:17 -0800

    Styling for login/register

--- src/Components/Pages/Page Components/Register.jsx
+++ src/Components/Pages/Page Components/Register.jsx
@@ -14,14 +14,27 @@ export const Register = (props) => {
 
     return (
         <>
-        <form method='POST' action='/register' onSubmit={handleSubmit}>
-            <label htmlFor='email'> Email </label>
-            <input value= {email} onChange={(e) => setLogInfo({ ...logInfo, email: e.target.value})} type= 'text' placeholder="youremail@email.com" id="user_email" required/>
-            <label htmlFor='password'> Password </label>
-            <input value={password} onChange={(e) => setLogInfo({ ...logInfo, password: e.target.value})} type= 'password' placeholder="****" id="user_password" required/>
-            <button> Create Account </button>
+        <div class='container flex flex-wrap justify-center items-center m-auto w-auto'>
+        <form class= "w-full max-w-lg " method='POST' action='/register' onSubmit={handleSubmit}>
+        <div class= 'flex flex-wrap justify-center items-center -mx-3 mb-6 '>
+            <div class= "w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label class= "block tracking-wide text-black-700 text-xl font-bold mb-2"for='user_email' htmlFor='email'> Email </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-rose-900 rounded 
+            py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" value= {email} onChange={(e) => setLogInfo({ ...logInfo, email: e.target.value})} type= 'text' placeholder="youremail@email.com" id="user_email" required/>
+            </div>
+            <div class= "w-full md:w-1/2 px-3">
+                <label class= "block tracking-wide text-black-700 text-xl font-bold mb-2" htmlFor='password'> Password </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-rose-900 rounded 
+            py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" value={password} onChange={(e) => setLogInfo({ ...logInfo, password: e.target.value})} type= 'password' placeholder="****" id="user_password" required/>
+            </div>
+            <div>
+                <button class= "appearance-none block w-full bg-gray-200 text-black-700 border border-rose-900 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"> Create Account </button>
+            </div>
+        </div>
         </form>
-            <button onClick={() => props.onFormSwitch('login')}> Already have an account? Log in here! </button>
+        </div>
+            <button class="appearance-none block w-full bg-gray-200 text-black-700 border border-rose-900 rounded py-3 px-4 leading-tight 
+              focus:outline-none focus:bg-white focus:border-gray-500" onClick={() => props.onFormSwitch('login')}> Already have an account? Log in here! </button>
         </>
 
     )
