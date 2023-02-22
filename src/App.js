81114331d4d25c8c0b21b1bf838f2e0ab5efaf4e
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-22 15:27:36 -0800

    adding styling

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import EventPage from './Components/Pages/EventPage';
 function App() {
 
   return (
-    <div >
-        <Routes className="bg-red-600">
+    <div className=" bg-red-600 min-h-screen">
+        <Routes>
           <Route path='/' element={<LandingPage />}/>  
           <Route path='/choose' element={<ChoosePage />}/>
           <Route path='/createevent' element={<CreateEventPage />}/>
--- src/Components/Pages/ChoosePage.jsx
+++ src/Components/Pages/ChoosePage.jsx
@@ -3,19 +3,21 @@ import ChooseCards from '../ChooseCards'
 
 function ChoosePage() {
   return (
-    <div class="container mx-auto my-4 px-4 bg-cyan-400" >
+    <div class="flex flex-row space-x-9 justify-center items-center  container mx-auto px-4 bg-red-600" >
       
-    <div className='flex justify-center items-center border border-slate-50 pt-5 '>
+    <div className='flex justify-center items-center my-20 border-slate-50 pt-5 '>
         <ChooseCards 
             path = '/createevent'
             text = ' Create Event'
+            description = 'Create your own event and begin your journey in meeting new people.'
         />
       </div>
         <br></br>
-      <div className='flex justify-center items-center border border-slate-50 pb-5'>
+      <div className='flex justify-center items-center my-20 border-slate-50 pt-5'>
         <ChooseCards 
             path = '/calender'
             text = 'Calender'
+            description = 'View other events for you to attend and explore'
         />
     </div>
     </div>
