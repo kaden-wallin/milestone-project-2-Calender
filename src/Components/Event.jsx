9f6d05c10b788342847a2dfb2cdd74cd09fe01a9
BuildTools <unconfigured@null.spigotmc.org>
2023-02-22 16:58:01 -0700

    currently having issues with the delete button but trying my best

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,43 +1,58 @@
 import { useState, useEffect } from 'react'
-import {  useLocation, useParams } from 'react-router-dom'
+import { useLocation, useParams, useNavigate } from 'react-router-dom'
 import GoBackBtn from './GoBackBtn'
+import events from '../../controller/event_controller'
 
+function Event(props) {
+  const [showButton, setShowButton] = useState(true)
+  const path = useLocation()
+  const { title } = useParams()
+  const navigate = useNavigate()
 
-function Event(props) { // we're going to pass some sort of props in the future
-    
-   const [showButton, setShowButton] = useState(true)
+  useEffect(() => {
+    if (path.pathname === '/calender') {
+      setShowButton(false)
+    }
+  }, [path.pathname])
 
-    const path = useLocation()
+  const handleDelete = () => {
+    events.delete('/:id', async (req, res) => {
+      try {
+          const deletedEvents = await Event.destroy({
+              where: {
+                  event_id: req.params.id
+              }
+          })
+          res.status(200).json({
+              message: `Successfully deleted ${deletedEvents} event(s)`
+          })
+      } catch(err) {
+          res.status(500).json(err)
+      }
+  })
+    // assuming the API call returns success, redirect to home page
+    navigate('/calender')
+  }
 
-    const { title } = useParams();
+  if (!props) {
+    console.log('id:', title)
+    return <div>Page Not Found</div>
+  }
 
-    // gets object based on the param name
-   // prevents btn from rendering in the calender
-     useEffect(() => {
-       if (path.pathname === '/calender') {
-         setShowButton(false);
-       }
-     }, [path.pathname])
-
-     // err message
-       if (!props) {
-        console.log('id:', title);
-          return <div>Page Not Found</div>
-       }
-     
   return (
     <div>
-         {showButton && <GoBackBtn />} 
+      {showButton && <GoBackBtn />} 
       <header>
         {props.title}
       </header>
       <main>
         <h2>
-        {props.location}
+          {props.location}
         </h2>
         <p>
           {props.date}
         </p>
+        <button onClick={handleDelete}>Delete</button>
       </main>
     </div>
   )
