aafe79c6dd334b0327e28fe9d8656f566060c496
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-22 13:07:31 -0800

    still broken :(

--- src/Components/Pages/CreateEventPage.jsx
+++ src/Components/Pages/CreateEventPage.jsx
@@ -1,41 +1,41 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-import { createClient } from '@supabase/supabase-js'
-import GoBackBtn from '../GoBackBtn';
+// import { createClient } from '@supabase/supabase-js'
+ import GoBackBtn from '../GoBackBtn';
 
-const supabaseUrl = process.env.DB_HOST
-const supabaseKey = process.env.DB_API_KEY
+// const supabaseUrl = process.env.DB_HOST
+// const supabaseKey = process.env.DB_API_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
 function CreateEventPage() {
-  async function handleSubmit(event) {
-    event.preventDefault()
+  // async function handleSubmit(event) {
+  //   event.preventDefault()
 
-    //EXTRACT THE FORM DATA
-    const formData = {
-      title: document.getElementById('event-title').value,
-      date: document.getElementById('choose-date').value,
-      location: document.getElementById('location').value,
-    }
+  //   //EXTRACT THE FORM DATA
+  //   const formData = {
+  //     title: document.getElementById('event-title').value,
+  //     date: document.getElementById('choose-date').value,
+  //     location: document.getElementById('location').value,
+  //   }
 
-    //MAKE THE API CALL
-    const { data, error } = await supabase
-      .from('events')
-      .insert(formData)
+  //   //MAKE THE API CALL
+  //   const { data, error } = await supabase
+  //     .from('events')
+  //     .insert(formData)
 
-    if (error) {
-      alert('Failed to add event')
-      console.error(error)
-    } else {
-      alert('Event added successfully!')
-    }
-  }
+  //   if (error) {
+  //     alert('Failed to add event')
+  //     console.error(error)
+  //   } else {
+  //     alert('Event added successfully!')
+  //   }
+  // }
 
   return (
     <div className=' text-center  m-auto w-auto items-center  justify-between font-bold bg-red-400'>
       <GoBackBtn />
-      <form onSubmit={handleSubmit} className= 'items-center justify-center rounded-md text-center t bg-gray-50 text-2xl'>
+      <form className= 'items-center justify-center rounded-md text-center t bg-gray-50 text-2xl'>
         <label htmlFor="event-title">Event Title: </label>
         <input id="event-title" type="text" />
         <label htmlFor="choose-date">Date: </label>
