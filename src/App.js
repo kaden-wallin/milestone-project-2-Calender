dec51a2cd583aac997b7413df14e29bf638eab83
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-22 13:03:13 -0800

    added calender functionality

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import LandingPage from './Components/Pages/LandingPage';
 import ChoosePage from './Components/Pages/ChoosePage';
 import CreateEventPage from './Components/Pages/CreateEventPage';
 import CalenderPage from './Components/Pages/CalenderPage';
-import Event from './Components/Event';
+import EventPage from './Components/Pages/EventPage';
 
 function App() {
+
   return (
-    <div className="">
-        <Routes>
+    <div >
+        <Routes className="bg-red-600">
           <Route path='/' element={<LandingPage />}/>  
           <Route path='/choose' element={<ChoosePage />}/>
           <Route path='/createevent' element={<CreateEventPage />}/>
           <Route path = '/calender'   element={<CalenderPage />}/>
-          <Route path='/event' element={<Event />}/> {/*The path should be some sort of params.name */}
+          <Route path='/event/:id' element={<EventPage />}/>
         </Routes>  
     </div>
   );
