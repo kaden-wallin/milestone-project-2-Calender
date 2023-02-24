4ed1cb8e658e8c8cd79327dbe4b1fae18c33d031
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-23 16:08:04 -0800

    styled Navbar more

--- src/Components/Pages/LandingPage.jsx
+++ src/Components/Pages/LandingPage.jsx
@@ -19,6 +19,8 @@ function LandingPage() {
         <Link to = '/choose' >
             <button className = 'm-14 inline-flex items-center justify-center rounded-full p-8 t mt-64 bg-red-600 text-white text-9xl text-red-600 hover:bg-white hover:text-red-500 hover:transform hover:scale-110 transition duration-300' type = 'button'>Start</button>
         </Link>
+      {/* 
+      // Decide as group if we wanna keep this link
       <div>
       {showForm &&
           (currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />)
@@ -30,7 +32,7 @@ function LandingPage() {
           Log In or Sign Up here
         </Link>
       )}
-        </div>
+        </div> */}
     </div>
   )
 }
