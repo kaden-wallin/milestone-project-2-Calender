ff3d377f5b3a72ed1f8099e4e4407ff66f45cd12
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-26 15:20:36 -0800

    changed urls

--- src/Components/Pages/EventPage.jsx
+++ src/Components/Pages/EventPage.jsx
@@ -13,7 +13,7 @@ function EventPage() {
 
     useEffect(() => {
       async function fetchData() {
-        const response = await fetch(`${process.env.URI}/api/events/${id}`);
+        const response = await fetch(`https://milestone-project-2-calender-backe-git-fa2279-mile-stone-2-team.vercel.app/api/events/${id}`);
         const eventData = await response.json();    
         setEventInfo(eventData);
       }
