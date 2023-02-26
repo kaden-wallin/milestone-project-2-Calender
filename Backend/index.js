4685bec40ef0e8b4bf7c07ce6037175fa39985ca
BuildTools <unconfigured@null.spigotmc.org>
2023-02-26 10:11:11 -0700

    maybe maybe maybe

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,10 @@ require('pg')
 
 // CONFIGURATION / MIDDLEWARE
 app.use(cors({
-    origin: "https://milestone-project-2-calender-updated-ri0ti32ob.vercel.app/",
+    origin: 'https://milestone-project-2-calender-updated-ri0ti32ob.vercel.app',
+  methods: 'GET,PUT,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }))
 
 app.use(express.json())
