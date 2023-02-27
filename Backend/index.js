368f8f858129cef229aba45bb62ac06f29a58610
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-26 20:23:57 -0800

    testing

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ require('pg')
 
 // CONFIGURATION / MIDDLEWARE
 app.use(cors({
-    origin: 'http://localhost:4001',
+    origin: 'https://milestone-project-2-calender-frontend4.vercel.app',
   methods: 'GET,PUT,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204
