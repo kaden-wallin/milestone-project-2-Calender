d7b5c8a09889c649b93c5457943c5df06b08fcf2
EmmanuelU <emmanuelurias60@icloud.com>
2023-02-26 16:07:03 -0800

    trying to fix

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ require('pg')
 
 // CONFIGURATION / MIDDLEWARE
 app.use(cors({
-    origin: 'https://milestone-project-2-calender-backend2.vercel.app,http://localhost/3300',
+    origin: 'https://milestone-project-2-calender-backend3.vercel.app',
   methods: 'GET,PUT,POST,DELETE',
   preflightContinue: false,
   optionsSuccessStatus: 204
